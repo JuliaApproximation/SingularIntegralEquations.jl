ce65eccf9cd5c14f28b806bee6c21f03c7adef11
Sheehan Olver <solver@mac.com>
2014-11-03 15:54:47 +1100

    fixed interval cauchy

--- src/intervalcauchy.jl
+++ src/intervalcauchy.jl
@@ -1,3 +1,5 @@
+import ApproxFun: JacobiRecurrenceOperator,dotu
+
 function cauchylegendre(z)
     [BasisFunctional(1),
         (JacobiRecurrenceOperator(0.,0.)-z)[2:end,:]]\[(log(z-1)-log(z+1))/(2π*im)]
@@ -6,6 +8,6 @@ end
 function cauchy(f::Fun{JacobiSpace},z)
     @assert space(f).a==0 && space(f).b==0
    cfs=cauchylegendre(z)
-   m=max(length(f),length(cfs))
-   dotu(cfs[1:m],f.coefficients[1:m]+0im) 
+   m=min(length(f),length(cfs))
+   dotu(cfs[1:m],f.coefficients[1:m]) 
 end
