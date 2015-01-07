8b93082ab6e216c3b03cdc7ce26ac8b6926cae69
Sheehan Olver <solver@mac.com>
2015-01-08 09:57:50 +1100

    Fixed typing of Hilbert to work with InterlaceOperator

--- src/Hilbert.jl
+++ src/Hilbert.jl
@@ -23,7 +23,7 @@ Hilbert(AS::ArraySpace,k::Integer)=HilbertWrapper(DiagonalArrayOperator(Hilbert(
 function Hilbert(S::PiecewiseSpace,k::Integer)
     @assert k==1 #TODO: Shouldn't need assertion.
     sp=vec(S)
-    C=BandedOperator[k==j?Hilbert(sp[k]):2im*Cauchy(sp[k],rangespace(Hilbert(sp[j]))) for j=1:length(sp),k=1:length(sp)]
+    C=BandedOperator{Complex{Float64}}[k==j?Hilbert(sp[k]):2im*Cauchy(sp[k],rangespace(Hilbert(sp[j]))) for j=1:length(sp),k=1:length(sp)]
     HilbertWrapper(interlace(C))
 end
 
