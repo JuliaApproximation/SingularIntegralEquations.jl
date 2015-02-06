38a228bba642bf779944671df210e6c82c3bee9f
Richard Mikael Slevinsky <Richard.Slevinsky@maths.ox.ac.uk>
2015-02-06 22:15:18 +0000

    scattering update for ProductFuns

--- examples/Scattering.jl
+++ examples/Scattering.jl
@@ -10,40 +10,33 @@
 using ApproxFun,SIE
 include("Scatteraux.jl")
 
-k = 50.
+k = 10.
 ω = 2π
 d = (1,-1)
 d = d[1]/hypot(d[1],d[2]),d[2]/hypot(d[1],d[2])
 ui(x,y) = exp(im*k*(d⋅(x,y)))
 
     dom = Interval(-1.,1.)
-    sp = Ultraspherical{0}(dom)
-    x = Fun(identity,dom)
-    w = 1/sqrt((dom.b-x)*(x-dom.a))
+    sp = Chebyshev(dom)
+    wsp = JacobiWeight(-.5,-.5,sp)
+    x = Fun(identity,sp)
     uiΓ,H0,S = Fun(x->ui(x,0),sp),Hilbert(dom,0),Σ(dom)
 
-#=
-    kd = int(k*(1+length(dom)/2))
-    FK0LR = LowRankFun((x,y)->FK0(k*(y-x)),sp,sp,2kd,2kd;maxrank=kd)
-    K0,Kim = -FK0LR/2pi,FK0LR/4
-    println("The ranks of K0 and Kim are: ",rank(K0),".")
-    Kr = LowRankFun((x,y)->(GK0(k.*(y-x))-FK0(k.*(y-x)).*(log(abs(k)/2)+γ))/2pi,sp,sp,2kd,2kd;maxrank=kd)
-    println("The rank of Kr is: ",rank(Kr),".")
-=#
-    FK0LR = Fun(x->FK0(k*x),[-2.,2.])
-    K0 = ProductFun(-coefficients(ProductFun(FK0LR))/2π,Ultraspherical{0}(),JacobiWeight(-.5,-.5,Ultraspherical{0}()))
-    Kim = ProductFun(coefficients(ProductFun(FK0LR))/4π,Ultraspherical{0}(),JacobiWeight(-.5,-.5,Ultraspherical{0}()))
-    #K0,Kim = -FK0LR/2pi,FK0LR/4
-    #println("The ranks of K0 and Kim are: ",rank(K0),".")
-    FKr = Fun(x->(GK0(k*x)-FK0(k*x).*(log(abs(k)/2)+γ))/2π,[-2.,2.])
-    Kr = ProductFun(coefficients(ProductFun(FKr))/π,Ultraspherical{0}(),JacobiWeight(-.5,-.5,Ultraspherical{0}()))
-    #Kr = LowRankFun((x,y)->(GK0(k.*(y-x))-FK0(k.*(y-x)).*(log(abs(k)/2)+γ))/2pi,sp,sp,2kd,2kd;maxrank=kd)
-    #println("The rank of Kr is: ",rank(Kr),".")
+    FK0LR = Fun(x->besselj0(k*x),Chebyshev([-length(dom),length(dom)]))
+    FKr = Fun(x->(GK0(k*x)-besselj0(k*x)*(log(abs(k)/2)+γ))/2π,Chebyshev([-length(dom),length(dom)]))
 
-    #L,f = H0[K0*w] + S[Kr*(w/π)] + im*S[Kim*(w/π)],uiΓ
+    K0 = ProductFun(-FK0LR/2π,sp,wsp)
+    Kim = ProductFun(FK0LR/4π,sp,wsp)
+    Kr = ProductFun(FKr/π,sp,wsp)
     L,f = H0[K0] + S[Kr] + im*S[Kim],uiΓ
 
     @time ∂u∂n = L\f
     println("The length of ∂u∂n is: ",length(∂u∂n))
 
     us(x,y) = Fun(t->-im/4.*hankelh1(0,k.*sqrt((x.-t).^2.+y.^2))*∂u∂n[t],ApproxFun.ArraySpace(sp,length(x)),length(∂u∂n)).coefficients[1:length(x)]
+#=
+dom1 = Interval(-2.5,-.5)∪Interval(.5,2.5)
+sp1 = Space(dom1)
+wsp1 = ApproxFun.PiecewiseSpace([JacobiWeight(-.5,-.5,sp1.spaces[i]) for i=1:length(sp1)])
+H1 = Hilbert(wsp1,1)
+=#
