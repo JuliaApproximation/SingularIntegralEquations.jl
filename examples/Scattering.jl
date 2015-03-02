4cdbc2be163c43fac22e3def0c8669c9a0930336
Richard Mikael Slevinsky <Richard.Slevinsky@maths.ox.ac.uk>
2015-03-02 14:20:01 +0000

    Tests pass up to Scattering example

--- examples/Scattering.jl
+++ examples/Scattering.jl
@@ -24,9 +24,9 @@ ui(x,y) = exp(im*k*(d⋅(x,y)))
 
     FK0LR = Fun(x->besselj0(k*x),Chebyshev([-length(dom),length(dom)]))
     FKr = Fun(x->1/2π*besselj0(k*x)*log(abs(x))-bessely0(k*abs(x))/4,Chebyshev([-length(dom),length(dom)]))
-    K0 = ProductFun(-FK0LR/2π,sp,wsp)
-    Kim = ProductFun(FK0LR/4π,sp,wsp)
-    Kr = ProductFun(FKr/π,sp,wsp)
+    K0 = SymmetricProductFun(-FK0LR/2π,sp,wsp)
+    Kim = SymmetricProductFun(FK0LR/4π,sp,wsp)
+    Kr = SymmetricProductFun(FKr/π,sp,wsp)
     L,f = H0[K0] + Σ[Kr] + im*Σ[Kim],uiΓ
 =#
 
@@ -43,28 +43,28 @@ ui(x,y) = exp(im*k*(d⋅(x,y)))
     # Kr11 division by π because S1 multiplies by π.
     FK011 = Fun(x->besselj0(k*x),Chebyshev([-length(dom[1]),length(dom[1])]))
     FKr11 = Fun(x->1/2π*besselj0(k*x)*log(abs(x))-bessely0(k*abs(x))/4,Chebyshev([-length(dom[1]),length(dom[1])]))
-    K011 = ProductFun(-FK011/2π,sp[1],wsp[1])
-    Kim11 = ProductFun(FK011/4π,sp[1],wsp[1])
-    Kr11 = ProductFun(FKr11/π,sp[1],wsp[1])
+    K011 = SymmetricProductFun(-FK011/2π,sp[1],wsp[1])
+    Kim11 = SymmetricProductFun(FK011/4π,sp[1],wsp[1])
+    Kr11 = SymmetricProductFun(FKr11/π,sp[1],wsp[1])
 
     # Region 1 -> 2
     F12r = Fun(x->-.25bessely0(k*abs(3.-x)),Chebyshev([-length(dom[1]),length(dom[1])]))
-    K12r = ProductFun(F12r/π,sp[2],wsp[1])
+    K12r = SymmetricProductFun(F12r/π,sp[2],wsp[1])
     F12im = Fun(x->.25besselj0(k*abs(3.-x)),Chebyshev([-length(dom[1]),length(dom[1])]))
-    K12im = ProductFun(F12im/π,sp[2],wsp[1])
+    K12im = SymmetricProductFun(F12im/π,sp[2],wsp[1])
 
     # Region 2 -> 1
     F21r = Fun(x->-.25bessely0(k*abs(3.-x)),Chebyshev([-length(dom[1]),length(dom[1])]))
-    K21r = ProductFun(F21r/π,sp[1],wsp[2])
+    K21r = SymmetricProductFun(F21r/π,sp[1],wsp[2])
     F21im = Fun(x->.25besselj0(k*abs(3.-x)),Chebyshev([-length(dom[1]),length(dom[1])]))
-    K21im = ProductFun(F21im/π,sp[1],wsp[2])
+    K21im = SymmetricProductFun(F21im/π,sp[1],wsp[2])
 
     # Region 2 -> 2
     FK022 = Fun(x->besselj0(k*x),Chebyshev([-length(dom[2]),length(dom[2])]))
     FKr22 = Fun(x->1/2π*besselj0(k*x)*log(abs(x))-bessely0(k*abs(x))/4,Chebyshev([-length(dom[2]),length(dom[2])]))
-    K022 = ProductFun(-FK022/2π,sp[2],wsp[2])
-    Kim22 = ProductFun(FK022/4π,sp[2],wsp[2])
-    Kr22 = ProductFun(FKr22/π,sp[2],wsp[2])
+    K022 = SymmetricProductFun(-FK022/2π,sp[2],wsp[2])
+    Kim22 = SymmetricProductFun(FK022/4π,sp[2],wsp[2])
+    Kr22 = SymmetricProductFun(FKr22/π,sp[2],wsp[2])
 
     L11 = H1[K011] + Σ1[Kr11] + im*Σ1[Kim11]
     L12 = Σ1[K12r] + im*Σ1[K12im]
