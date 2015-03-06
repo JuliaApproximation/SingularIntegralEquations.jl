20c1f4be4392bfac4542af13fd40a1f7267501c6
Richard Mikael Slevinsky <Richard.Slevinsky@maths.ox.ac.uk>
2015-03-06 17:04:43 +0000

    Laplace now works
    but it’s not using OffHilbert operators… will see how I can fix this.

--- examples/Laplace.jl
+++ examples/Laplace.jl
@@ -9,25 +9,52 @@
 
 using ApproxFun,SIE
 
-z_0 = -1.5+1.0im
+z_0 = 0.0+im
 ui(x,y) = 1/2π*SIE.logabs(complex(x,y)-z_0)
 
 
+    N = 2
     dom = Interval([-2.5,-0.5])∪Interval([0.5,2.5])
-    sp = ApproxFun.PiecewiseSpace(map(ApproxFun.ChebyshevDirichlet{1,1},dom.domains))
-    wsp = ApproxFun.PiecewiseSpace([JacobiWeight(-.5,-.5,sp.spaces[i]) for i=1:length(sp)])
-    uiΓ,H0 = Fun(x->ui(x,0.0),sp),0.5Hilbert(wsp,0)
-    L,f = H0,uiΓ
+#    sp = ApproxFun.PiecewiseSpace(map(ApproxFun.ChebyshevDirichlet{1,1},dom.domains))
+    sp = Space(dom)
+    wsp = ApproxFun.PiecewiseSpace([JacobiWeight(-.5,-.5,sp.spaces[i]) for i=1:N])
+
+    csp = [CauchyWeight{0}(sp[i]⊗sp[i]) for i=1:N]
+    cwsp = [CauchyWeight{0}(sp[i]⊗wsp[i]) for i=1:N]
+#=
+    dom = Interval([-1.,1.])
+    sp = ApproxFun.ChebyshevDirichlet{1,1}(dom)
+    wsp = JacobiWeight(-.5,-.5,sp)
+=#
+    #H0 = Fun(x->ui(x,0.0),sp),0.5Hilbert(wsp,0)
+
+    uiΓ,⨍ = Fun(x->ui(x,0.0),sp),PrincipalValue(wsp)
+
+    g1(x,y) = 1/2
+    g3(x,y) = 1/2π*SIE.logabs(y-x)
+
+    G = Array(GreensFun,N,N)
+    for i=1:N,j=1:N
+        if i == j
+            G[i,i] = GreensFun([ProductFun(g1,csp[i])])
+        else
+            G[i,j] = GreensFun([ProductFun(g3,sp[i],sp[j];method=:convolution)])
+        end
+    end
+
+    L,f = ⨍[G],uiΓ
 
     @time ∂u∂n = L\f
     println("The length of ∂u∂n is: ",length(∂u∂n))
 
-    ∂u∂nv = vec(∂u∂n)
+#us(x,y) = -1/2π*logkernel(∂u∂n,complex(x,y))
 
+
+    ∂u∂nv = vec(∂u∂n)
     function us(x,y)
-        ret = -1/2π*logkernel(∂u∂nv[1],complex(x,y))
+        ret = -1/2π*logkernel(Fun(∂u∂nv[1].coefficients,wsp[1]),complex(x,y))
         for i=2:length(∂u∂nv)
-            ret -= 1/2π*logkernel(∂u∂nv[i],complex(x,y))
+            ret -= 1/2π*logkernel(Fun(∂u∂nv[i].coefficients,wsp[i]),complex(x,y))
         end
         ret
     end
