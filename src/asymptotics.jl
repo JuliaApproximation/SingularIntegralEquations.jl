3c4b144780f996115db71c8e5485953c747c9b5d
Sheehan Olver <solver@mac.com>
2015-04-29 09:23:33 +1000

    Added LogKernel for Laurent/JacobiWeight{Chebyshev}

--- src/asymptotics.jl
+++ src/asymptotics.jl
@@ -10,6 +10,7 @@ function LogKernelAsymptotics(S::Fourier,k)
     end
 end
 
+LogKernelAsymptotics(S::Laurent,k)=LogKernelAsymptotics(Fourier(domain(S)),k)*Conversion(S,Fourier(domain(S)))
 
 
 
@@ -24,3 +25,13 @@ function LogKernelAsymptotics(S::JacobiWeight{ChebyshevDirichlet{1,1}},k)
     end
 end
 
+
+function LogKernelAsymptotics(S::JacobiWeight{Chebyshev},k)
+    @assert S.α==S.β==-0.5
+    d=domain(f)
+    if k==-1  # coefficient in front of logabs term
+        CompactFunctional([π*length(d)/2],S)
+    else
+        error("Not implemented")
+    end
+end
