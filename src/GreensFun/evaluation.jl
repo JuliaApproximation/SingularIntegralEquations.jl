d50dcd18c34be2a54a35d08c1ac4abd5c116bc1d
Richard Mikael Slevinsky <Richard.Slevinsky@maths.ox.ac.uk>
2015-05-29 19:35:34 +0100

    reorganize fast GreensFun eval routines

--- src/GreensFun/evaluation.jl
+++ src/GreensFun/evaluation.jl
@@ -2,43 +2,23 @@
 for (Func,Len) in ((:(Base.sum),:complexlength),(:linesum,:length))
     @eval begin
         function $Func(G::Function,u::Fun{JacobiWeight{Chebyshev}},z)
-            d,α,β,n=domain(u),u.space.α,u.space.β,length(u)#nextpow2(length(u))
+            d,α,β,n=domain(u),u.space.α,u.space.β,length(u)
             vals,t = ichebyshevtransform(u.coefficients),points(d,n)
-            #vals,t = ichebyshevtransform(pad(u.coefficients,n)),points(d,n)
-            #p = plan_chebyshevtransform(complex(vals))
-            #X = map(z->chebyshevtransform(G(z,t).*vals,p)[1],z)
             if α == β == -0.5
                 return 0.5*$Len(d)*map(z->mean(G(z,t).*vals),z)*π
-            end
-      #=
-            if α ≤ -1.0 || β ≤ -1.0
-                fs = Fun(f.coefficients,f.space.space)
-                return Inf*0.5*$Len(d)*(sign(fs[d.a])+sign(fs[d.b]))/2
-            elseif α == β == -0.5
-                return 0.5*$Len(d)*f.coefficients[1]*π
-            elseif α == β == 0.5
-                return 0.5*$Len(d)*(n ≤ 2 ? f.coefficients[1]/2 : f.coefficients[1]/2 - f.coefficients[3]/4)*π
-            elseif α == 0.5 && β == -0.5
-                return 0.5*$Len(d)*(n == 1 ? f.coefficients[1] : f.coefficients[1] + f.coefficients[2]/2)*π
-            elseif α == -0.5 && β == 0.5
-                return 0.5*$Len(d)*(n == 1 ? f.coefficients[1] : f.coefficients[1] - f.coefficients[2]/2)*π
             else
-                c = zeros(eltype(f),n)
-                c[1] = 2.^(α+β+1)*gamma(α+1)*gamma(β+1)/gamma(α+β+2)
-                if n > 1
-                    c[2] = c[1]*(α-β)/(α+β+2)
-                    for i=1:n-2
-                        c[i+2] = (2(α-β)*c[i+1]-(α+β-i+2)*c[i])/(α+β+i+2)
-                    end
-                end
-                return 0.5*$Len(d)*dotu(f.coefficients,c)
+                sp,p = space(u),plan_chebyshevtransform(complex(vals))
+                map(z->$Func(Fun(chebyshevtransform(G(z,t).*vals,p),sp),z),z)
             end
-        =#
         end
-        $Func{F<:Fun}(G::Function,u::Vector{F},z)=mapreduce(u->$Func(G,u,z),+,u)
-        $Func{P<:PiecewiseSpace,T}(G::Function,u::Fun{P,T},z)=$Func(G,vec(u),z)
-        $Func{PS<:PolynomialSpace}(G::Function,f::Fun{JacobiWeight{PS}},z)=$Func(G,Fun(f,Chebyshev(domain(f))),z)
     end
+end
+
+function logkernel(G::Function,u::Fun{JacobiWeight{Chebyshev}},z)
+    sp,n=space(u),length(u)
+    vals,t = ichebyshevtransform(u.coefficients),points(sp,n)
+    p = plan_chebyshevtransform(complex(vals))
+    return map(z->logkernel(Fun(chebyshevtransform(G(z,t).*vals,p),sp),z),z)
 end
 
 function Base.sum{S<:Union(Fourier,Laurent)}(G::Function,u::Fun{S},z)
@@ -57,17 +37,16 @@ function linesum{S<:Union(Fourier,Laurent)}(G::Function,u::Fun{S},z)
     map(z->mean(G(z,t).*vals),z)*length(d)
 end
 
-function logkernel(G::Function,u::Fun{JacobiWeight{Chebyshev}},z)
-    sp,n=space(u),length(u)
-    vals,t = ichebyshevtransform(u.coefficients),points(sp,n)
-    p = plan_chebyshevtransform(complex(vals))
-    return map(z->logkernel(Fun(chebyshevtransform(G(z,t).*vals,p),sp),z),z)
-end
 function logkernel{S<:Union(Fourier,Laurent)}(G::Function,u::Fun{S},z)
     sp,n=space(u),length(u)
     vals,t = values(u),points(sp,n)
     return map(z->logkernel(Fun(transform(sp,G(z,t).*vals),sp),z),z)
 end
-logkernel{F<:Fun}(G::Function,u::Vector{F},z)=mapreduce(u->logkernel(G,u,z),+,u)
-logkernel{P<:PiecewiseSpace,T}(G::Function,u::Fun{P,T},z)=logkernel(G,vec(u),z)
-logkernel{PS<:PolynomialSpace}(G::Function,f::Fun{JacobiWeight{PS}},z)=logkernel(G,Fun(f,Chebyshev(domain(f))),z)
+
+for Func in (:(Base.sum),:linesum,:logkernel)
+    @eval begin
+        $Func{F<:Fun}(G::Function,u::Vector{F},z)=mapreduce(u->$Func(G,u,z),+,u)
+        $Func{P<:PiecewiseSpace,T}(G::Function,u::Fun{P,T},z)=$Func(G,vec(u),z)
+        $Func{PS<:PolynomialSpace}(G::Function,f::Fun{JacobiWeight{PS}},z)=$Func(G,Fun(f,Chebyshev(domain(f))),z)
+    end
+end
