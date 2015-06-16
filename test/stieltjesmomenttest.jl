ef87f5f6c51697dfeb10d641701952eb634a67de
Sheehan Olver <solver@mac.com>
2015-06-17 05:12:05 +1000

    test cauchy on [-1.,0.,1.] versus [-1.,1.]

--- test/stieltjesmomenttest.jl
+++ test/stieltjesmomenttest.jl
@@ -17,3 +17,6 @@ x=Fun()
 @test_approx_eq stieltjesmoment(JacobiWeight(0.5,0.,Jacobi(0.5,0.)),1,z) sum(sqrt(1+x)/(z-x))
 @test_approx_eq stieltjesmoment(JacobiWeight(0.5,0.,Jacobi(0.5,0.)),2,z) sum(x*sqrt(1+x)/(z-x))
 
+
+
+@test_approx_eq cauchy(sqrt(1-Fun(identity,[-1.,0.,1.])^2),1.+1.im) cauchy(sqrt(1-Fun()^2),1.+1.im)
