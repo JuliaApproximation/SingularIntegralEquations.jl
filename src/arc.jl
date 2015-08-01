7965a3e4e33d36e49a635d37994b146fc88ac3b0
Sheehan Olver <solver@mac.com>
2015-08-02 09:53:49 +1000

    cauchyintegral on arcs

--- src/arc.jl
+++ src/arc.jl
@@ -24,3 +24,19 @@ function Hilbert{LS,RR<:Arc,TT}(sp::MappedSpace{LS,RR,TT},k::Integer)
     H=Hilbert(csp)+(1/π)*Stieltjes(csp,tocanonical(sp,Inf))
     HilbertWrapper(SpaceOperator(H,MappedSpace(domain(sp),domainspace(H)),MappedSpace(domain(sp),rangespace(H))),k)
 end
+
+
+
+## cauchyintegral
+
+function cauchyintegral{LS,RR<:Arc,TT}(w::Fun{MappedSpace{LS,RR,TT}},z)
+    d=domain(w)
+    g=Fun(w.coefficients,w.space.space)*ApproxFun.fromcanonicalD(d,Fun())
+    cauchyintegral(g,tocanonical(d,z))-
+    cauchyintegral(g,tocanonical(d,Inf))-
+    sum(w)*(-log(z-fromcanonical(d,Inf)))/(-2π*im)
+end
+
+
+
+
--- test/CurveTest.jl
+++ test/CurveTest.jl
@@ -1,3 +1,7 @@
+using ApproxFun, SingularIntegralEquations, Base.Test
+
+## quadratic
+
 a=1+10*im;b=2-6*im
 d=Curve(Fun(x->1+a*x+b*x^2))
 
@@ -7,3 +11,16 @@ w=sqrt(abs(first(d)-x))*sqrt(abs(last(d)-x))
 
 
 @test_approx_eq cauchy(w,2.) sum(w/(x-2.))/(2π*im)
+
+
+## Arc
+
+d=exp(im*Interval(0.1,0.2))
+x=Fun(d)
+w=sqrt(abs(first(d)-x))*sqrt(abs(last(d)-x))
+
+
+z=10.;
+@test_approx_eq sum(w/(x-z))/(2π*im) cauchy(w,z)
+@test_approx_eq sum(w*log(z-x))/(-2π*im) cauchyintegral(w,z)
+
