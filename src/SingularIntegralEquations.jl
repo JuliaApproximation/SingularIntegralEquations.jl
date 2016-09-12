263ec9e1e1d824e675fc80cfb3513b0979cf858b
Sheehan Olver <solver@mac.com>
2016-09-12 20:18:02 +1000

    update for new ApproxFun

--- src/SingularIntegralEquations.jl
+++ src/SingularIntegralEquations.jl
@@ -27,7 +27,7 @@ import ApproxFun: bandinds,SpaceOperator,bilinearform,linebilinearform,dotu,
                   Fun, ProductFun, LowRankFun, mappoint, JacobiZ,
                   real, UnivariateSpace, RealUnivariateSpace, setdomain, eps, choosedomainspace, isapproxinteger, BlockOperator,
                   ConstantSpace,ReOperator,DirectSumSpace,TupleSpace, ZeroSpace,
-                  DiagonalInterlaceOperator, LowRankPertOperator, LaurentDirichlet, setcanonicaldomain,
+                  LowRankPertOperator, LaurentDirichlet, setcanonicaldomain,
                   IntervalCurve,PeriodicCurve, reverseorientation, op_eltype, @wrapper, mobius,
                   defaultgetindex, WeightSpace, pochhammer, spacescompatible, ∞, LowRankMatrix, refactorsvd!
 
