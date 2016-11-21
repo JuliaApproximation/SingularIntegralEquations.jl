3bfc8bcb17042c472b1811e7a55b6e3e9f964ef4
Sheehan Olver <solver@mac.com>
2016-11-22 08:15:42 +1100

    move out of memory test

--- test/fulltests.jl
+++ test/fulltests.jl
@@ -1,3 +1,12 @@
 
 include("runtests.jl")
+
+## Memory CurveTests
+
+d=exp(im*Interval(0.1,0.2))
+x=Fun(d)
+w=1/(sqrt(abs(first(d)-x))*sqrt(abs(last(d)-x)))
+testbandedoperator(Hilbert(space(w)))
+
+
 include("ExamplesTest.jl")
